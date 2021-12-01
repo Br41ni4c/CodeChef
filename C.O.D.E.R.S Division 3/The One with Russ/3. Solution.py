736524578ee5a891018f2d06a90ee1be6ba57b05
Br41ni4c <89293594+Br41ni4c@users.noreply.github.com>
2021-12-02 00:44:36 +0530

    Update 3. Solution.py

--- C.O.D.E.R.S Division 3/The One with Russ/3. Solution.py
+++ C.O.D.E.R.S Division 3/The One with Russ/3. Solution.py
@@ -1,1 +1,14 @@
-
+import math
+n = int(input())
+for i in range(n):
+    n1,x,k = map(int,input().split())
+    a = [int(i) for i in input().split()]
+    b = [int(i) for i in input().split()]
+    c = 0
+    for i in range(n1):
+        if(abs(a[i]-b[i])<=k):
+            c+=1
+    if(c>=x):
+        print("YES")
+    else:
+        print("NO")
